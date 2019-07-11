793c5521b64979c721cab24d1165835983f16b83
Charles Boadu <ekowcharles@gmail.com>
2019-07-11 09:55:20 -0500

    Change app launch text to avoid confusion in understanding ports

--- exerciseii/index.js
+++ exerciseii/index.js
@@ -11,5 +11,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(8000, () => {
-  console.log("Dockerizable app listening on port 8000!");
+  console.log("Dockerizable app listening to connections!");
 });
